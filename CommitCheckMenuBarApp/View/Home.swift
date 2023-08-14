641e60687c57b50e8d4c8450b65815e0f1bab923
sunghun <leedool3003@gmail.com>
2023-08-14 16:01:24 +0900

    :hammer: [chore] setup menu bar

--- CommitCheckMenuBarApp/View/Home.swift
+++ CommitCheckMenuBarApp/View/Home.swift
@@ -8,16 +8,74 @@
 import SwiftUI
 
 struct Home: View {
+	
+	@State var currentTab: String = "test1"
+	@Namespace var animation
+	
     var body: some View {
 		VStack {
-			Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+			CustomSegmentdControl()
+				.padding()
+			
+			Spacer()
+			
+			Text("")
+			
+			Spacer()
+			
+			HStack {
+				Button {
+					
+				} label: {
+					Image(systemName: "gearshape.fill")
+				}
+				
+				Spacer()
+				
+				Button {
+					
+				} label: {
+					Image(systemName: "power")
+				}
+			}
+			.padding(.horizontal)
+			.padding(.vertical, 10)
+			.background(Color.black)
 		}
 		.frame(width: 320, height: 450)
+		.background(Color("BG"))
+		.preferredColorScheme(.dark)
+		.buttonStyle(.plain)
     }
+	
+	@ViewBuilder
+	func CustomSegmentdControl() -> some View {
+		HStack(spacing: 0) {
+			ForEach(["test1", "test2"], id:\.self) { tab in
+				Text(tab)
+					.fontWeight(currentTab == tab ? .semibold : .regular)
+					.foregroundColor(currentTab == tab ? .white : .gray)
+					.frame(maxWidth: .infinity)
+					.padding(.vertical, 6)
+					.background {
+						if currentTab == tab {
+							RoundedRectangle(cornerRadius: 8, style: .continuous)
+								.fill(Color("TAB"))
+								.matchedGeometryEffect(id: "TAB", in: animation)
+						}
+					}
+					.contentShape(Rectangle())
+					.onTapGesture {
+						withAnimation {
+							currentTab = tab
+						}
+					}
+			}
+		}
+		.padding()
+		.background {
+			Color.black
+				.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+		}
+	}
 }
-
-//struct Home_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Home()
-//    }
-//}
