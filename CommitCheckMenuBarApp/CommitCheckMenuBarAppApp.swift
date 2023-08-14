064d95937b673301d57aac6239ee954025197357
sunghun <leedool3003@gmail.com>
2023-08-14 15:20:03 +0900

    :hammer: [chore] setup popover view

--- CommitCheckMenuBarApp/CommitCheckMenuBarAppApp.swift
+++ CommitCheckMenuBarApp/CommitCheckMenuBarAppApp.swift
@@ -9,9 +9,47 @@ import SwiftUI
 
 @main
 struct CommitCheckMenuBarAppApp: App {
+	
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class AppDelegate: NSObject, ObservableObject, NSApplicationDelegate {
+	
+	@Published var statusItem: NSStatusItem?
+	@Published var popover = NSPopover()
+	
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		setUpMacMenu()
+	}
+	
+	func setUpMacMenu() {
+		popover.animates = true
+		popover.behavior = .transient
+		
+		popover.contentViewController = NSViewController()
+		popover.contentViewController?.view = NSHostingView(rootView: Home())
+		
+		popover.contentViewController?.view.window?.makeKey()
+		
+		statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+		if let menuButton = statusItem?.button {
+			menuButton.image = .init(systemSymbolName: "dollarsign.circle.fill", accessibilityDescription: nil)
+			menuButton.action = #selector(menuButtonAction(sender:))
+		}
+	}
+	
+	@objc func menuButtonAction(sender: AnyObject) {
+		if popover.isShown {
+			popover.performClose(sender)
+		} else {
+			if let menuButton = statusItem?.button {
+				popover.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: .minY)
+			}
+		}
+	}
 }
---
+++ CommitCheckMenuBarApp/View/Home.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  CommitCheckMenuBarApp
+//
+//  Created by Kim SungHun on 2023/08/14.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+		VStack {
+			Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		}
+		.frame(width: 320, height: 450)
+    }
+}
+
+//struct Home_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Home()
+//    }
+//}
