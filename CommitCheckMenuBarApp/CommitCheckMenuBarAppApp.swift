093c3b8350637dc18209e87bcbb1edc46490e895
sunghun <leedool3003@gmail.com>
2023-08-14 14:41:47 +0900

    :tada: init project

---
+++ CommitCheckMenuBarApp/CommitCheckMenuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitCheckMenuBarAppApp.swift
+//  CommitCheckMenuBarApp
+//
+//  Created by Kim SungHun on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct CommitCheckMenuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
